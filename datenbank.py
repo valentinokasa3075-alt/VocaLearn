c18abaee3847e1549c9aa11447ec7af73559a612
Erdu19 <erduanvlasi16@gmail.com>
2026-04-24 00:37:43 +0200

    Speichern und Laden Vokabeln

---
+++ datenbank.py
@@ -0,0 +1,21 @@
+import json
+import os
+
+# absoluter Pfad zu dem Ordner, in dem diese Datei liegt
+BASIS_ORDNER = os.path.dirname(os.path.abspath(__file__))
+
+# kompletter Pfad zur JSON-Datei
+DATEI_PFAD = os.path.join(BASIS_ORDNER, "vocalearn_data.json")
+
+
+def lade_vokabeln():
+    try:
+        with open(DATEI_PFAD, "r", encoding="utf-8") as datei:
+            return json.load(datei)
+    except:
+        return []
+
+
+def speichere_vokabeln(vokabeln):
+    with open(DATEI_PFAD, "w", encoding="utf-8") as datei:
+        json.dump(vokabeln, datei, indent=4, ensure_ascii=False)
